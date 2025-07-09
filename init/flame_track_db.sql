031b46a6317f5730379b7404af8f01a8100bb28d
iiesteban <e.ortiz06@ufromail.cl>
2025-07-09 05:07:35 -0400

    refactor: update init sql

--- init/flame_track_db.sql
+++ init/flame_track_db.sql
@@ -4,60 +4,42 @@
 -- #                                                               #
 -- #################################################################
 
-CREATE TABLE `plans` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `name` VARCHAR(45) NOT NULL,
-  `description` VARCHAR(255) NULL, 
-  `price` VARCHAR(45) NOT NULL, 
-  `panel_id` VARCHAR(255) NOT NULL COMMENT 'ID del dashboard en ThingsBoard para este plan',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE plans (
+  id SERIAL PRIMARY KEY,
+  name VARCHAR(45) NOT NULL,
+  description VARCHAR(255),
+  price VARCHAR(45) NOT NULL,
+  panel_id VARCHAR(255) NOT NULL -- comentario
+);
 
-CREATE TABLE `devices` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `token_access` VARCHAR(255) NOT NULL UNIQUE COMMENT 'Token de acceso del dispositivo (del QR)',
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE devices (
+  id SERIAL PRIMARY KEY,
+  token_access VARCHAR(255) NOT NULL UNIQUE
+);
+-- ####################################################################
+
+CREATE TABLE users (
+  id SERIAL PRIMARY KEY,
+  username VARCHAR(45) NOT NULL UNIQUE,
+  email VARCHAR(255) NOT NULL UNIQUE,
+  password VARCHAR(255) NOT NULL,
+  plan_id INT NULL,
+  CONSTRAINT fk_users_plans FOREIGN KEY(plan_id) REFERENCES plans(id)
+);
 
 -- ####################################################################
 
-CREATE TABLE `users` (
-  `id` INT NOT NULL AUTO_INCREMENT,
-  `username` VARCHAR(45) NOT NULL UNIQUE,
-  `email` VARCHAR(255) NOT NULL UNIQUE,
-  `password` VARCHAR(255) NOT NULL COMMENT 'Guardar siempre contraseñas hasheadas',
-  `plan_id` INT NOT NULL,
-  PRIMARY KEY (`id`),
-  INDEX `fk_users_plans_idx` (`plan_id` ASC),
-  CONSTRAINT `fk_users_plans`
-    FOREIGN KEY (`plan_id`)
-    REFERENCES `plans` (`id`)
-    ON DELETE NO ACTION
-    ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
-
--- ####################################################################
-
-CREATE TABLE `user_devices` (
-  `user_id` INT NOT NULL,
-  `device_id` INT NOT NULL,
-  PRIMARY KEY (`user_id`, `device_id`),
-  INDEX `fk_user_devices_devices_idx` (`device_id` ASC),
-  CONSTRAINT `fk_user_devices_users`
-    FOREIGN KEY (`user_id`)
-    REFERENCES `users` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION,
-  CONSTRAINT `fk_user_devices_devices`
-    FOREIGN KEY (`device_id`)
-    REFERENCES `devices` (`id`)
-    ON DELETE CASCADE
-    ON UPDATE NO ACTION
-) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+CREATE TABLE user_devices (
+  user_id INT NOT NULL,
+  device_id INT NOT NULL,
+  PRIMARY KEY (user_id, device_id),
+  CONSTRAINT fk_user_devices_users FOREIGN KEY(user_id) REFERENCES users(id) ON DELETE CASCADE,
+  CONSTRAINT fk_user_devices_devices FOREIGN KEY(device_id) REFERENCES devices(id) ON DELETE CASCADE
+);
 
 -- ####################################################################
 
 -- Se crea los planes
-INSERT INTO `plans` (`name`, `price`,`description`,`panel_id`) VALUES
-('free', "GRATIS",'Acceso básico a las funciones principales', 'ID_DEL_DASHBOARD_GRATUITO'),
-('pro', "50.000",'Funciones avanzadas como historial', 'ID_DEL_DASHBOARD_PROFESIONAL');
+INSERT INTO plans (name, price, description, panel_id) VALUES
+('free', 'GRATIS', 'Acceso básico a las funciones principales', 'ID_DEL_DASHBOARD_GRATUITO'),
+('pro', '50.000', 'Funciones avanzadas como historial', 'ID_DEL_DASHBOARD_PROFESIONAL');
