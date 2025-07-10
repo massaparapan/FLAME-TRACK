2588835affb0bcc6194e20c138388183c0013a20
iiesteban <e.ortiz06@ufromail.cl>
2025-07-10 03:48:28 -0400

    refactor: simplify navigation structure and remove unused screens

--- frontend/src/app.tsx
+++ frontend/src/app.tsx
@@ -1,45 +1,18 @@
-import DashboardScreen from "@/src/screens//DashboardScreen";
-import MainMenu from "@/src/screens/MenuScreen";
-import { NavigationContainer } from "@react-navigation/native";
-import { createNativeStackNavigator } from "@react-navigation/native-stack";
-import React from "react";
-import { StatusBar } from "react-native";
-import { colors } from "./constans/colors";
-import PlansScreen from "./screens/PlansScreen";
-import { RootStackParamList } from "./types/navigation";
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createNativeStackNavigator } from '@react-navigation/native-stack';
 
-const Stack = createNativeStackNavigator<RootStackParamList>();
+import AuthScreen from './screens/AuthScreen';
+import Layout from './screens/_layout';
+
+const Stack = createNativeStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <StatusBar/>
-      
-      <Stack.Navigator
-        initialRouteName="MainMenu"
-  
-        screenOptions={{
-          headerTitleAlign: 'center',
-          headerStyle: { backgroundColor: colors.primary[500] },
-          headerTintColor: "#ffffff",
-          headerTitleStyle: { fontWeight: "bold" },
-        }}
-      >
-        <Stack.Screen
-          name="MainMenu"
-          component={MainMenu}
-          options={{ title: "Menú Principal" }}
-        />
-        <Stack.Screen
-          name="Dashboard"
-          component={DashboardScreen}
-          options={{ title: "Dashboard" }}
-        />
-        <Stack.Screen
-          name="Plans"
-          component={PlansScreen}
-          options={{ title: "Planes" }}
-        />
+      <Stack.Navigator initialRouteName="Auth" screenOptions={{ headerShown: false }}>
+        <Stack.Screen name="Auth" component={AuthScreen} />
+        <Stack.Screen name="Main" component={Layout} />
       </Stack.Navigator>
     </NavigationContainer>
   );
